48fa45e12e3ac21518feb6db0c9a9f808677c540
Elukwal <elukwal3@gmail.com>
2018-09-18 11:10:35 +0300

    Auto-generated commit

--- src/app/time-counter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeCounterPipe } from './time-counter.pipe';
-
-describe('TimeCounterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeCounterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
